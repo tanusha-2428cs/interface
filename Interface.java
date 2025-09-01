a0d836c729deef8588db3b9f38da342cc43a0e12
Tanusha Tyagi <tanusha.2428cs583@kiet.edu>
2025-09-01 18:24:33 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,5 +1,22 @@
-public class Interface {
-    public static void main
-    
-    
+interface Inter1{
+   int x=112;
+     void method1();
+}
+interface Inter2{
+    int x=115;
+    void method3();
+}
+public class Test1 implements Inter1,Inter2 {
+    public static void main(String[] args) {
+        System.out.println("Main Entry Point");
+        System.out.println(Inter1.x);
+        int x=113;
+        System.out.println(x);
+    }
+    public void method1(){
+        System.out.println("This is my method1");
+    }
+    public void method3(){
+        System.out.println("This is my method3");
+    }
 }
